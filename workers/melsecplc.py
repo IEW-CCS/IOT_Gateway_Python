547474917c5f2dcf52e68d2778d83c1f0cd04304
英璟 陳 <e30803@hotmail.com>
2019-07-29 16:55:53 +0800

    add report alarm to host

--- workers/melsecplc.py
+++ workers/melsecplc.py
@@ -7,19 +7,17 @@ import datetime
 import os
 import logging
 
-
 from HslCommunication import MelsecMcNet
 from HslCommunication import SoftBasic
 from mqtt import MqttMessage
-
+from config import settings
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from pytz import utc
 
 from multiprocessing import Queue as queue
-
-
 from workers.base import BaseWorker
+
 import logger
 
 _LOGGER = logger.get(__name__)
@@ -68,6 +66,7 @@ class PLCDevice:
       self.message_sent = False
     print("PLCDevice: set_status() --> message_sent", self.message_sent)
 
+
   def _timeout(self):
     if self.available:
       return self.worker.available_timeout
@@ -89,10 +88,13 @@ class PLCDevice:
     melsecNet = MelsecMcNet(ip_addr, port)
     if melsecNet.ConnectServer().IsSuccess == False:
       print("PLCDevice: get_peripheral --> connect falied")
+      _LOGGER.info("PLCDevice: get_peripheral --> connect falied")
       return None
     else:
       print("PLCDevice: get_peripheral --> connect successed")
+      _LOGGER.info("PLCDevice: get_peripheral --> connect successed")
       return melsecNet
+
 
   def read_data(self, addr):
     print("PLCDevice --> read_data: config is ", addr)
@@ -113,12 +115,11 @@ class PLCDevice:
 
       if ad['DATA_ADDR'][0] not in PLC_ADDR_ENUM:
         print("Address check error: ", ad['DATA_ADDR'])
+        _LOGGER.error ("Address check error: "+ str(ad['DATA_ADDR']))
         return None
     
       if ad['DATA_TYPE'] in PLC_DTYPE_ENUM:
         print("Type check OK: ", ad['DATA_TYPE'])
-        
-
         
         if ad['DATA_TYPE'] == PLC_DTYPE_INT or ad['DATA_TYPE'] == PLC_DTYPE_UINT:
           strFuncName = "self.p_device.Read"+ad['DATA_TYPE'].title() + str(ad['DATA_LENGTH'])
@@ -150,6 +151,7 @@ class PLCDevice:
 #        result = locals()[strFuncName](ad['DATA_ADDR'], ad['DATA_LENGTH'])
       else:
         print("Type check NG")
+        _LOGGER.error("Type check NG")
         return None
 
     ret_json.update({'EDC_Data':edc_array})
@@ -178,6 +180,13 @@ class PLCDevice:
     return messages
 
 class MelsecplcWorker(BaseWorker):
+
+  melsecPlc_Setting =  settings['manager']['workers']['melsecplc']
+  gateway_id = None
+  device_name = None
+  if 'topic_subscription' in melsecPlc_Setting:
+      _,_, gateway_id, device_name, cmd_type, cmd = melsecPlc_Setting['topic_subscription'].split('/')
+
   # Default values
   devices = {}
   # Payload that should be send when device is available
@@ -194,20 +203,25 @@ class MelsecplcWorker(BaseWorker):
   flag_started = False #Flag to control BleDeviceStatus object creation
   count = 0
   
-  ReadData_Topic = "/IEW/{gateway}/{device}/ReplyData"
+
+  # Defined Puhlish MQTT Topic 
+  ReadData_Topic  =  "/IEW/{gateway}/{device}/ReplyData"
   HeartBeat_Topic = "/IEW/{gateway}/{device}/Status/HeartBeat"
+  Alarm_Topic = "/IEW/{gateway}/{device}/Status/Alarm"
+
   ReadData_job_id = '{}_interval_job'.format("ReadData")
-  gateway_id ="gateway001"
-  device_name="device001"
   Version = "1.0"
   Status = "Init"
   
   Job_queue = queue(maxsize = 10)
 
+  _LOGGER.info("Version = "+ Version ) 
   _LOGGER.info("MelsecPLC Class Initial Funished")
+  _LOGGER.info("Setting Gateway_ID  = "+ gateway_id + ", Device_ID = " + device_name )
   _LOGGER.info("ReadData_Topic = "+ ReadData_Topic)
   _LOGGER.info("HeartBeat_Topic = "+ HeartBeat_Topic)
-  _LOGGER.info("Version = "+Version )  
+ 
+
     
   def __init__(self, command_timeout, **kwargs):
   
@@ -216,10 +230,13 @@ class MelsecplcWorker(BaseWorker):
     self._scheduler.add_job(self.Read_PLC_Data, 'interval', seconds=10, id=self.ReadData_job_id)
     self._scheduler.start()
 
+    # Apschedule 不要紀錄 Logger 
     logging.getLogger('apscheduler.executors.default').propagate = False
-
+   
     self.Status = "Init"
     _LOGGER.info("MelsecplcWorker --> starts = " +  self.Status)  
+
+    self.report_alarm( '0000', 'Info', 'MelsecplcWorker is Initial')
     
    
   def run(self, mqtt):
@@ -229,8 +246,11 @@ class MelsecplcWorker(BaseWorker):
        if self.Job_queue.empty() == True:
            continue
        else:
-           SendOutMQTT = self.Job_queue.get()
-           mqtt.publish(SendOutMQTT)
+          try :
+             SendOutMQTT = self.Job_queue.get()
+             mqtt.publish(SendOutMQTT) 
+          except Exception as ee:
+             logger.log_exception(_LOGGER, 'MelsecplcWorker --> Publish Error Msg : %s', str(ee))
              
              
   def create_devices(self):
@@ -260,19 +280,21 @@ class MelsecplcWorker(BaseWorker):
        if self.count > 65535:
           self.count =1
         
-       #_LOGGER.debug("MelsecplcWorker --> status_update enters count = "+ self.count)
-
+       _LOGGER.debug("MelsecplcWorker --> ReadPLC Data enters count = "+ str( self.count))
        sendout_topic =  self.ReadData_Topic.replace("{gateway}", self.gateway_id ).replace("{device}", self.device_name)
-       self.Status = "Run"
-       
+
+       # 成功讀取狀態改為Run
        try:
           for status in self.last_status:
              json_msg = status.read_data(self.addr_array)
              status.set_status(status is not None)
              self.Job_queue.put([MqttMessage(topic=sendout_topic, payload=json_msg)])
-       except:
-           print("Melsecplcworker Excepting")
-           self.Status = "Down"
+             _LOGGER.info("MelsecplcWorker --> Report Read PLC Data: " + json_msg )
+             self.Status = "Run"
+       except Exception as ee:
+             logger.log_exception(_LOGGER, 'MelsecplcWorker --> Read Data Excepting. Msg : %s', str(ee))
+             self.Status = "Down"
+             # Read Data Error Send Error to MQTT
         
     else:
       _LOGGER.info("MelsecplcWorker --> status_update: Waiting for Collect Command!")
@@ -285,37 +307,56 @@ class MelsecplcWorker(BaseWorker):
       cmd_start = json.loads(payload)
         
       _LOGGER.info("MelsecplcWorker --> read_payload_cmd_start: payload = "+ payload)
-     # _LOGGER.info("MelsecplcWorker --> read_payload_cmd_start: cmd_start = "+ cmd_start)
-     # _LOGGER.info("MelsecplcWorker --> read_payload_cmd_start: cmd_start['Device_Info] = "+ cmd_start['Device_Info'])
+      _LOGGER.info("MelsecplcWorker --> read_payload_cmd_start: cmd_start = "+ str(cmd_start))
+      _LOGGER.info("MelsecplcWorker --> read_payload_cmd_start: cmd_start['Device_Info] = "+ str(cmd_start['Device_Info']))
       sendout_topic =  topic + "/Ack"
       
-      
       if cmd_start['Cmd_Type'] =="Start" :
+          connect_status = 'OK'
+
           self.devices.update({device_name:cmd_start['Device_Info']})
+
+          # 連線設定完成
+          if not self.start_to_collect:
+            self.create_devices()
+
+          for status in self.last_status:
+             if status.p_device == None:
+                 connect_status = 'NG'
+
           ret_json = {}
-          ret_json.update({'Cmd_Result':"OK"})
+          ret_json.update({'Cmd_Result': connect_status})
           ret_json.update({'Trace_ID':cmd_start['Trace_ID']})
           json_msg = json.dumps(ret_json)
           self.Job_queue.put([MqttMessage(topic=sendout_topic, payload=json_msg)])
-          self.Status = "Ready"
-      
-          if not self.start_to_collect:
-            self.create_devices()
+
+          if connect_status == 'NG':
+             self.Status = "Down"
+             self.report_alarm( '1001', 'ERROR', 'Connected PLC Faild')
+          else:
+             self.Status = "Ready"
+             self.report_alarm( '0001', 'INFO', 'Connected PLC successed')
+          
       else:
           ret_json = {}
           ret_json.update({'Cmd_Result':"NG"})
           ret_json.update({'Trace_ID':cmd_start['Trace_ID']})
           json_msg = json.dumps(ret_json)
           self.Job_queue.put([MqttMessage(topic=sendout_topic, payload=json_msg)])
+          # 補送Alarm to MQTT
+          self.report_alarm( '1002', 'ERROR', 'Cmd_Type not Start')
         
-    except ValueError:
-      logger.log_exception(_LOGGER, 'Ignoring invalid new interval: %s', payload)
+    except Exception as ee:
+      logger.log_exception(_LOGGER, 'Cmd Start Exception Error Msg : %s', str(ee))
+      self.Status = "Down"
       ret_json = {}
       ret_json.update({'Cmd_Result':"NG"})
       ret_json.update({'Trace_ID':cmd_start['Trace_ID']})
       json_msg = json.dumps(ret_json)
       self.Job_queue.put([MqttMessage(topic=sendout_topic, payload=json_msg)])
-      self.Status = "Down"
+       # 補送Alarm to MQTT
+      self.report_alarm( '1003', 'ERROR', 'Cmd_Type Start happened exception error :' + str(ee))
+
      
 
   def read_payload_cmd_readdata(self, device_name,  payload, topic):
@@ -324,7 +365,7 @@ class MelsecplcWorker(BaseWorker):
       cmd_read = {}
       cmd_read = json.loads(payload)
       
-      #_LOGGER.info("MelsecplcWorker --> read_payload_cmd_parameter: cmd_parameter = "+ cmd_read)
+      _LOGGER.info("MelsecplcWorker --> read_payload_cmd_parameter: cmd_parameter = "+ str(cmd_read))
       sendout_topic =  topic + "/Ack"
       
       if cmd_read['Cmd_Type'] =="Collect" :
@@ -342,6 +383,7 @@ class MelsecplcWorker(BaseWorker):
           ret_json.update({'Trace_ID':cmd_read['Trace_ID']})
           json_msg = json.dumps(ret_json)
           self.Job_queue.put([MqttMessage(topic=sendout_topic, payload=json_msg)])
+
           
       else:
           ret_json = {}
@@ -349,15 +391,20 @@ class MelsecplcWorker(BaseWorker):
           ret_json.update({'Trace_ID':cmd_read['Trace_ID']})
           json_msg = json.dumps(ret_json)
           self.Job_queue.put([MqttMessage(topic=sendout_topic, payload=json_msg)])
+          _LOGGER.error("MelsecplcWorker --> read_payload_cmd_NG tpye not Collect, cmd_read_type = "+ str(cmd_read['Cmd_Type']))
+          # 補送Alarm to host
+          self.report_alarm( '1004', 'ERROR', 'Type not Collect')
       
-    except ValueError:
-      logger.log_exception(_LOGGER, 'Ignoring invalid new interval: %s', payload)
+    except Exception as ee:
+      logger.log_exception(_LOGGER, 'Cmd Start Exception Error Msg : %s', str(ee))
       ret_json = {}
       ret_json.update({'Cmd_Result':"NG"})
       ret_json.update({'Trace_ID':cmd_read['Trace_ID']})
       json_msg = json.dumps(ret_json)
       self.Job_queue.put([MqttMessage(topic=sendout_topic, payload=json_msg)])
       self.Status = "Down"
+      self.report_alarm( '1005', 'ERROR', 'Cmd_Type Collect happened exception error :' + str(ee))
+
       
     
   def read_payload_parameter_request(self, device_name, payload):
@@ -378,7 +425,6 @@ class MelsecplcWorker(BaseWorker):
       
     _LOGGER.info("MelsecplcWorker --> Heartbit Report")
     now = datetime.datetime.now()
-    
     sendout_topic =  self.HeartBeat_Topic.replace("{gateway}", self.gateway_id ).replace("{device}", self.device_name)
 
     HB_json = {}
@@ -389,7 +435,6 @@ class MelsecplcWorker(BaseWorker):
     _LOGGER.debug("Heartbit Report : " + json_msg)
 
     ret =[]
-
     messages = []
     messages.append(MqttMessage(topic = sendout_topic, payload = json_msg))
 
@@ -397,12 +442,30 @@ class MelsecplcWorker(BaseWorker):
 
     return ret
 
+  def report_alarm(self, code, level, desc):
+      
+   
+    now = datetime.datetime.now()
+    report_alarm_Topic =  self.Alarm_Topic.replace("{gateway}", self.gateway_id ).replace("{device}", self.device_name)
+
+    Alarm_json = {}
+    Alarm_json.update({'AlarmCode': code})
+    Alarm_json.update({'AlarmLevel': level})
+    Alarm_json.update({'AlarmApp': 'WORKER'})
+    Alarm_json.update({'DateTime': now.strftime("%Y%m%d%H%M%S%f")[:-3]})
+    Alarm_json.update({'AlarmDesc': desc})
+    json_msg = json.dumps(Alarm_json)
+
+    self.Job_queue.put([MqttMessage(topic = report_alarm_Topic, payload = json_msg)])
+    _LOGGER.error ("MelsecplcWorker --> Report Alarm to Host Msg : " + str(json_msg))
+    
+
   def on_command(self, topic, value):
       
-    value = value.decode('utf-8')
+    value = value.decode('utf-8')\
+
+    # 設定改在其他地方
     _,_, gateway_id, device_name, cmd_type, cmd = topic.split('/')
-    self.gateway_id = gateway_id
-    self.device_name = device_name
 
     if cmd_type == "Cmd":
         
@@ -418,6 +481,9 @@ class MelsecplcWorker(BaseWorker):
         self.read_payload_cmd_readdata(device_name, value, topic)
       elif cmd == "Stop":
         self.cmd_stop(value)
+        _LOGGER.error("MelsecplcWorker --> on_command = "+ cmd + "Stop Collect Data")
+        self.Status = "Idle"
+
       elif cmd == "OTA":
         OTA_json = {}
         OTA_json.update({'Version': self.Version})
